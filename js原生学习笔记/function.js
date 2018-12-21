14e7895261b509c546f86d3fc1a0c2849b795468
xiadong.zhang <xiadong.zhang@aispeech.com>
2018-12-21 16:22:59 +0800

    save

--- js原生学习笔记/function.js
+++ js原生学习笔记/function.js
@@ -1,83 +1,75 @@
 //只要是和function相关的都可以记录在这里
-
-/* 
-  (1): Hello world
-  说明: 匿名函数写法,直接将函数作为参数传入,实现回调
-  回调方法"function(word){console.log(word)"总是在execute执行后执行
-  具体场景:暂缺
-*/ 
-// function execute(value, someFunction) {
-//   setTimeout(function(){
-//     someFunction(value+" world");
-//   }, 1500);
-// }
-// execute("Hello" , function(word){ console.log(word) });
-
-
-
-
-/* 
-  (2): Hello world
-  说明: 和(1)完全等价
-  唯一的区别是将匿名函数变为函数名为fun1的函数
-  这里的execute("Hello" , fun1),需特别注意,这里传入的应该是函数名,或者函数本身;而不能写成execute("Hello" , fun1('some string'))
-  原因是传入的就不是函数了,直接执行了fun1这个函数,完全错误的写法
-*/
-
-//正确的写法
-  // function fun1(word) {
-  //   console.log(word)
-  // }
-  // function execute(value, someFunction) {
-  //   setTimeout(function(){
-  //     someFunction(value+" world");
-  //   }, 1500);
-  // }
-  // execute("Hello" , fun1);
-
-//错误的写法
-  // function fun1(word) {
-  //   console.log(word)
-  // }
-  // function execute(value, someFunction) {
-  //   setTimeout(function(){
-  //     someFunction(value+" world");
-  //   }, 1500);
-  // }
-  // execute("Hello" , fun1('test'));
  
-/* 
-  (3): Hello world zhang
-  说明: 和(1)完全等价
-  fun1执行后返回了一个函数,所以execute传入参数fun1('Hello')
-  这样多包了一层之后,能针对不同需求,传入多个不同的fun1参数
-*/
+// (1): Hello world
+//   说明: 匿名函数写法,直接将函数作为参数传入,实现回调
+//   回调方法"function(word){console.log(word)"总是在execute执行后执行
+//   具体场景:暂缺 
+  function execute(value, someFunction) {
+    setTimeout(function(){
+      someFunction(value+" world");
+    }, 1500);
+  }
+  execute("Hello" , function(word){ console.log(word) });
 
-//写法1 Hello world zhang
-// function fun1(para1) {
-//   return function(para2) {
-//     console.log(para2 + para1)
-//   }
-// }
-// function execute(value, someFunction) {
-//   setTimeout(function(){
-//     someFunction(value + " world");
-//   }, 1500);
-// };
-// execute("Hello" , fun1(' zhang'));
+
+// (2): Hello world
+//   说明: 和(1)完全等价
+//   唯一的区别是将匿名函数变为函数名为fun1的函数
+//   这里的execute("Hello" , fun1),需特别注意,这里传入的应该是函数名,或者函数本身;而不能写成execute("Hello" , fun1('some string'))
+//   原因是传入的就不是函数了,直接执行了fun1这个函数,完全错误的写法
+
+// 正确的写法
+  function fun1(word) {
+    console.log(word)
+  }
+  function execute(value, someFunction) {
+    setTimeout(function(){
+      someFunction(value+" world");
+    }, 1500);
+  }
+  execute("Hello" , fun1);
+
+// 错误的写法
+  function fun1(word) {
+    console.log(word)
+  }
+  function execute(value, someFunction) {
+    setTimeout(function(){
+      someFunction(value+" world");
+    }, 1500);
+  }
+  execute("Hello" , fun1('test'));
+  
+// (3): Hello world zhang
+    // 说明: 和(1)完全等价
+    // fun1执行后返回了一个函数,所以execute传入参数fun1('Hello')
+    // 这样多包了一层之后,能针对不同需求,传入多个不同的fun1参数
+
+// 写法1 Hello world zhang
+  function fun1(para1) {
+    return function(para2) {
+      console.log(para2 + para1)
+    }
+  }
+  function execute(value, someFunction) {
+    setTimeout(function(){
+      someFunction(value + " world");
+    }, 1500);
+  };
+  execute("Hello" , fun1(' zhang'));
 
 //写法2 zhang Hello world
-// function fun1(para1) {
-//   return function(para2) {
-//     console.log(para2 + para1)
-//   }
-// }
-// function execute(value, someFunction) {
-//   setTimeout(function(){
-//     someFunction(value + " world")('zhang');
-//   }, 1500);
-// };
-// execute("Hello" , fun1);
+function fun1(para1) {
+  return function(para2) {
+    console.log(para2 + para1)
+  }
+}
+function execute(value, someFunction) {
+  setTimeout(function(){
+    someFunction(value + " world")('zhang');
+  }, 1500);
+};
+execute("Hello" , fun1);
 
 
 
--- js原生学习笔记/promise.js
+++ js原生学习笔记/promise.js
@@ -9,63 +9,63 @@
   说明: Promise简单例子
   resolve只能带一个参数;若想传递多个参数，可以包装成一个对象
 */ 
-// function test(timeOut) {
-//   return function(resolve, reject) {
-//     console.log('set timeout to: ' + timeOut + ' seconds.');
-//     setTimeout(function () {
-//       if (timeOut < 1) {
-//         console.log('call resolve()...');
-//         resolve({'one': 'ONE',
-//         'two': 'TWO'});
-//       }
-//       else {
-//         console.log('call reject()...');
-//           reject('timeout in ' + timeOut + ' seconds.');
-//       }
-//     }, timeOut * 1000);
-//   }
-// }
-// var timeOut = Math.random() * 2;
-// var p1 = new Promise(test(timeOut));
-// var p2 = p1.then(function (result) {
-//   console.log('成功：' + result.one);
-// });
-// var p3 = p2.catch(function (reason) {
-//   console.log('失败：' + reason);
-// });
+function test(timeOut) {
+  return function(resolve, reject) {
+    console.log('set timeout to: ' + timeOut + ' seconds.');
+    setTimeout(function () {
+      if (timeOut < 1) {
+        console.log('call resolve()...');
+        resolve({'one': 'ONE',
+        'two': 'TWO'});
+      }
+      else {
+        console.log('call reject()...');
+          reject('timeout in ' + timeOut + ' seconds.');
+      }
+    }, timeOut * 1000);
+  }
+}
+var timeOut = Math.random() * 2;
+var p1 = new Promise(test(timeOut));
+var p2 = p1.then(function (result) {
+  console.log('成功：' + result.one);
+});
+var p3 = p2.catch(function (reason) {
+  console.log('失败：' + reason);
+});
 
 /* 
   (2): 
   说明: 多个Promise按顺序执行
   第一个promise执行成功后，再执行下一个，知道结束。若其中有一个promise执行失败（reject），直接执行catch;不需要在每个promise中都做catch判断，只要在最后做一个判断即可
 */ 
-// function multiply(input) {
-//   return new Promise(function (resolve, reject) {
-//     console.log('calculating ' + input + ' x ' + input + '...');
-//     setTimeout(()=> {
-//       reject('乘法出错')
-//     }, 500);
-//   });
-// }
-// function add(input) {
-//   return new Promise(function (resolve, reject) {
-//     console.log('calculating ' + input + ' + ' + input + '...');
-//       setTimeout(() => {
-//         resolve(input+input)
-//       }, 500);
-//   });
-// }
-// var p = new Promise(function (resolve, reject) {
-//   console.log('start new Promise...');
-//   resolve(12);
-// });
-// p.then(add)
-// .then(multiply)
-// .then(function (result) {
-//   console.log('Got value: ' + result);
-// }).catch(function (reason) {
-//   console.log('失败：' + reason);
-// });
+function multiply(input) {
+  return new Promise(function (resolve, reject) {
+    console.log('calculating ' + input + ' x ' + input + '...');
+    setTimeout(()=> {
+      reject('乘法出错')
+    }, 500);
+  });
+}
+function add(input) {
+  return new Promise(function (resolve, reject) {
+    console.log('calculating ' + input + ' + ' + input + '...');
+      setTimeout(() => {
+        resolve(input+input)
+      }, 500);
+  });
+}
+var p = new Promise(function (resolve, reject) {
+  console.log('start new Promise...');
+  resolve(12);
+});
+p.then(add)
+.then(multiply)
+.then(function (result) {
+  console.log('Got value: ' + result);
+}).catch(function (reason) {
+  console.log('失败：' + reason);
+});
 
 
 
@@ -75,52 +75,51 @@
   多个promise的按顺序执行，不需要在每个promise中都做catch判断，只要在最后做一个判断即可
   疑问：Promise.resolve（）
 */ 
+var func1 = function(callback){
+  var a = 1
+  setTimeout(function(a){
+    console.log(a);
+    typeof(callback) !== 'function' || callback();
+  }, 1500);
+};
 
-// var func1 = function(callback){
-//   var a = 1
-//   setTimeout(function(a){
-//     console.log(a);
-//     typeof(callback) !== 'function' || callback();
-//   }, 1500);
-// };
-
-// var promisify = function(func){
-//   return function(){
-//     return new Promise(function(resolve){
-//       func(resolve);
-//     });
-//   }
-// }
-// var func_arr = [promisify(func1)];
-// func_arr.reduce(function(cur, next) {
-//   return cur.then(next);
-//   }, Promise.resolve()).then(function() {
-//     console.log('job finished');
-// });
+var promisify = function(func){
+  return function(){
+    return new Promise(function(resolve){
+      func(resolve);
+    });
+  }
+}
+var func_arr = [promisify(func1)];
+func_arr.reduce(function(cur, next) {
+  return cur.then(next);
+  }, Promise.resolve()).then(function() {
+    console.log('job finished');
+});
 
 /* 
   (4): 
   说明: 对比下面俩段代码
 */
 //普通的最简单回调例子
-// function add(input) {
-//   console.log(input)
-// }
-// function execute(someFunction) {
-//   someFunction("world");
-// }
-// execute(add);
+function add(input) {
+  console.log(input)
+}
+function execute(someFunction) {
+  someFunction("world");
+}
+execute(add);
 
 //使用了promise的回调例子，.then和execute是一样的，都是函数的调用。
 //参数问题： resolve的值会作为add的参数，直接传给add
-// function add(input) {
-//   console.log(input)
-// }
-// var p = new Promise(function (resolve, reject) {
-//   resolve({'one': 'ONE',
-//           'two': 'TWO'});
-// });
-// p.then(add)
+function add(input) {
+  console.log(input)
+}
+var p = new Promise(function (resolve, reject) {
+  resolve({'one': 'ONE',
+          'two': 'TWO'});
+});
+p.then(add)
 
 
 /* 
@@ -131,11 +130,36 @@
     return f2();
   }).then(f3);
 */
-// var p = new Promise(function (resolve, reject) {
-//   resolve(1);
-// });
-// p.then(function (data1) {
-//   return data1;
-// }).then((data2)=>{
-//   console.log(data2)
-// });
+var p = new Promise(function (resolve, reject) {
+  resolve(1);
+});
+p.then(function (data1) {
+  return data1;
+}).then((data2)=>{
+  console.log(data2)
+});
+
+
+/* 
+  (6): 
+  说明: 特殊写法：resolve一个新的promise
+  const p1 = new Promise(function (resolve, reject) {
+  });
+
+  const p2 = new Promise(function (resolve, reject) {
+    resolve(p1);
+  })
+  p2的resolve方法将p1作为参数
+  这时,p1的状态将决定p2的状态，也就是说p2的状态这时已经失效了，只需考虑p1的状态
+  如下例子，过程如下:
+  1.执行p2这个promise，一秒后执行resolve（p1）
+  2.因为p1也是一个promise，此时不是将p1作为参数传递给result，而是等待p1的状态;换句话说，此时的p2.then已经和p2的状态没关系了
+  3.又过两秒后， p1返回reject，继而执行catch方法;若p1返回的是resolve，则执行then方法。
+*/
+const p1 = new Promise(function (resolve, reject) {
+  setTimeout(() => reject(new Error('fail')), 3000)
+})
+const p2 = new Promise(function (resolve, reject) {
+  setTimeout(() => resolve(p1), 1000)
+})
+p2.then(result => console.log(result)).catch(error => console.log(error))
