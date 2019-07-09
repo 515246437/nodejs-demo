077c1974726d13642956ade8ed96595fab27882b
xiadong.zhang <xiadong.zhang@aispeech.com>
2019-07-09 10:21:12 +0800

    save

--- testServer.js
+++ testServer.js
@@ -22,17 +22,7 @@ app.get('/workorders', function (req, res) {
     {
       "workOrders":
         [
-          { "orderid": 1, "saleOrder": "SO1", "material": "A", "quantity": 100, "workQuantity": 110, "startTime": "2018-02-07T16:20:00+0800", "endTime": "2018-03-13 16:20:00+08" },
-          { "orderid": 2, "saleOrder": "SO1", "material": "B", "quantity": 200, "workQuantity": 210, "startTime": "2018-03-13T16:20:00+0800", "endTime": "2018-03-27 16:20:00+08" },
-          { "orderid": 3, "saleOrder": "SO2", "material": "C", "quantity": 150, "workQuantity": 160, "startTime": "2018-03-27T16:20:00+0800", "endTime": "2018-04-27 16:20:00+08" },
-          { "orderid": 3, "saleOrder": "SO2", "material": "C", "quantity": 150, "workQuantity": 160, "startTime": "2018-03-27T16:20:00+0800", "endTime": "2018-04-27 16:20:00+08" },
-          { "orderid": 3, "saleOrder": "SO2", "material": "C", "quantity": 150, "workQuantity": 160, "startTime": "2018-03-27T16:20:00+0800", "endTime": "2018-04-27 16:20:00+08" },
-          { "orderid": 1, "saleOrder": "SO1", "material": "A", "quantity": 100, "workQuantity": 110, "startTime": "2018-02-07T16:20:00+0800", "endTime": "2018-03-13 16:20:00+08" },
-          { "orderid": 2, "saleOrder": "SO1", "material": "B", "quantity": 200, "workQuantity": 210, "startTime": "2018-03-13T16:20:00+0800", "endTime": "2018-03-27 16:20:00+08" },
-          { "orderid": 3, "saleOrder": "SO2", "material": "C", "quantity": 150, "workQuantity": 160, "startTime": "2018-03-27T16:20:00+0800", "endTime": "2018-04-27 16:20:00+08" },
-          { "orderid": 3, "saleOrder": "SO2", "material": "C", "quantity": 150, "workQuantity": 160, "startTime": "2018-03-27T16:20:00+0800", "endTime": "2018-04-27 16:20:00+08" },
-          { "orderid": 3, "saleOrder": "SO2", "material": "C", "quantity": 150, "workQuantity": 160, "startTime": "2018-03-27T16:20:00+0800", "endTime": "2018-04-27 16:20:00+08" },
-          { "orderid": 1, "saleOrder": "SO1", "material": "A", "quantity": 100, "workQuantity": 110, "startTime": "2018-02-07T16:20:00+0800", "endTime": "2018-03-13 16:20:00+08" }
+          { "orderid": 1}
         ]
     })
 });
@@ -42,38 +32,7 @@ app.get('/products', function (req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.send(
     {
-      "products":[
-          {
-              "ID":1,
-              "name":"A",
-              "imageURL":"/images/A.png"
-          },
-          {
-              "ID":2,
-              "name":"B",
-              "imageURL":"/images/B.png"
-          },
-          {
-            "ID":3,
-            "name":"C",
-            "imageURL":"/images/C.png"
-          },
-          {
-            "ID":4,
-            "name":"D",
-            "imageURL":"/images/D.png"
-        },
-        {
-          "ID":5,
-          "name":"A",
-          "imageURL":"/images/A.png"
-        },
-        {
-            "ID":6,
-            "name":"B",
-            "imageURL":"/images/B.png"
-        }
-      ]
+      "products":[{"ID":1,}]
     }
   )
 });
